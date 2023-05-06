45d1d05a40835cd0e329c875743f3613d7f9127f
JAYPEE CANLAS <jpcanlas.student@ua.edu.ph>
2023-05-06 11:37:43 +0800

    first commit

---
+++ client/src/components/create.js
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+ 
+export default function Create() {
+ const [form, setForm] = useState({
+   name: "",
+   address: "",
+   course: "",
+   yearLevel: "",
+   id: "",
+ });
+ const navigate = useNavigate();
+ 
+ // These methods will update the state properties.
+ function updateForm(value) {
+   return setForm((prev) => {
+     return { ...prev, ...value };
+   });
+ }
+ 
+ // This function will handle the submission.
+ async function onSubmit(e) {
+   e.preventDefault();
+ 
+   // When a post request is sent to the create url, we'll add a new record to the database.
+   const newPerson = { ...form };
+ 
+   await fetch("http://localhost:5000/record/add", {
+     method: "POST",
+     headers: {
+       "Content-Type": "application/json",
+     },
+     body: JSON.stringify(newPerson),
+   })
+   .catch(error => {
+     window.alert(error);
+     return;
+   });
+ 
+   setForm({ name: "", address: "", course: "", yearLevel: "", id: "" });
+   navigate("/");
+ }
+ 
+ // This following section will display the form that takes the input from the user.
+ return (
+   <div>
+     <h3>Create New Record</h3>
+     <form onSubmit={onSubmit}>
+       <div className="form-group">
+         <label htmlFor="name">Name</label>
+         <input
+           type="text"
+           className="form-control"
+           id="name"
+           value={form.name}
+           onChange={(e) => updateForm({ name: e.target.value })}
+         />
+       </div>
+       <div className="form-group">
+         <label htmlFor="address">Address</label>
+         <input
+           type="text"
+           className="form-control"
+           id="address"
+           value={form.address}
+           onChange={(e) => updateForm({ address: e.target.value })}
+         />
+       </div>
+       <div className="form-group">
+         <label htmlFor="course">Course</label>
+         <input
+           type="text"
+           className="form-control"
+           id="course"
+           value={form.course}
+           onChange={(e) => updateForm({ course: e.target.value })}
+         />
+       </div>
+       <div className="form-group">
+         <label htmlFor="yearLevel">Year level</label>
+         <input
+           type="text"
+           className="form-control"
+           id="yearLevel"
+           value={form.yearLevel}
+           onChange={(e) => updateForm({ yearLevel: e.target.value })}
+         />
+       </div>
+       <div className="form-group">
+         <label htmlFor="id">Id</label>
+         <input
+           type="text"
+           className="form-control"
+           id="id"
+           value={form.id}
+           onChange={(e) => updateForm({ id: e.target.value })}
+         />
+       </div>
+       <div className="form-group">
+         <input
+           type="submit"
+           value="Create person"
+           className="btn btn-primary"
+         />
+       </div>
+     </form>
+   </div>
+ );
+}
